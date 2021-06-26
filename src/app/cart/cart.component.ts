54b83a4912ecf7e7b9ae72a28fd377dd8f1a85cb
matsushinDB11 <matsushin35@gmail.com>
2021-06-26 20:56:02 +0900

    finish start

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder} from "@angular/forms";
 import {CartService} from "../cart.service";
 
 @Component({
@@ -7,10 +8,21 @@ import {CartService} from "../cart.service";
   styleUrls: ['./cart.component.css']
 })
 export class CartComponent implements OnInit {
-  items = this.cartService.getItems();
   constructor(
-    private cartService: CartService
+    private cartService: CartService,
+    private formBuilder: FormBuilder,
   ) { }
+  items = this.cartService.getItems();
+  checkoutForm = this.formBuilder.group({
+    name: '',
+    address: ''
+  });
+  onSubmit(): void {
+    // フォーム処理
+    this.items = this.cartService.clearCart();
+    console.warn('注文は送信されました', this.checkoutForm.value);
+    this.checkoutForm.reset();
+  }
 
   ngOnInit(): void {
   }
